5d7a1df42c912111026566adf602c8ebad6b8533
Sean M. Collins <sean@coreitpro.com>
2016-04-19 12:38:19 -0400

    OpenStack API based inventory discovery
    Use the OpenStack REST APIs to find all the services that run
    in a cluster.
    
    Serves as a concrete example of the ServiceDiscovery abstract class.
    This class is intended to serve as the base class for different
    discovery mechanisms. Other discovery mechanisms include Puppet,
    Ansible, or other provisioning tools.

--- kostyor/inventory/discover.py
+++ kostyor/inventory/discover.py
@@ -1,31 +1,95 @@
+import abc
 import re
 
-from keystoneauth1.identity import v2
-from keystoneauth1 import session
-from keystoneclient.v2_0 import client
+
+from keystoneclient.v2_0 import client as k_client
+from novaclient import client as n_client
+from neutronclient.v2_0 import client as mutnauq_client
+
+OS_COMPUTE_API_VERSION = 2
 
 
-class KeystoneDiscovery(object):
-    """ Uses the Keystone REST API to discover services """
+@abc.ABCMeta
+class ServiceDiscovery(object):
 
-    def __init__(self, username, password, tenant_name, auth_url):
-        sess = session.Session(auth=v2.Password(username=username,
-                                                password=password,
-                                                tenant_name=tenant_name,
-                                                auth_url=auth_url))
-        self.keystone = client.Client(session=sess)
+    @abc.abstractmethod
+    def discover(self):
+        """
+        Returns a list of services, and the host they are on.
 
-    def discover_nova_api_host(self):
-        # Determine Nova API endpoint
-        nova_endpoint = self.keystone.endpoints.find(
-            service_id=self.keystone.services.find(type="compute").id)
+        Example:
 
-        # TODO(sc68cal) God help us if someone is using an IPv6 address in
+            [(u'devstack-1.coreitpro.com', u'nova-conductor'),
+             (u'devstack-1.coreitpro.com', u'nova-cert'),
+             (u'devstack-1.coreitpro.com', u'nova-scheduler'),
+             (u'devstack-1.coreitpro.com', u'nova-consoleauth'),
+             (u'devstack-1.coreitpro.com', u'nova-compute'),
+             (u'devstack-2.coreitpro.com', u'nova-compute'),
+             (u'devstack-3.coreitpro.com', u'nova-compute')]
+        """
+        pass
+
+
+class OpenStackServiceDiscovery(ServiceDiscovery):
+    """
+    Users of this class need to create a keystone session and pass it
+    into the constructor.
+
+    Example:
+        from keystoneauth1.identity import v2
+        from keystoneauth1 import session
+        sess = session.Session(auth=v2.Password(username='admin',
+            password='changeme', tenant_name='demo',
+            auth_url="http://192.168.1.246:5000/v2.0/"))
+
+        cluster_discovery = ServiceDiscovery(sess)
+        cluster_discovery.discover()
+
+    """
+    def __init__(self, keystone_session):
+        self.session = keystone_session
+
+    def discover(self):
+        services = []
+        services.extend(self.discover_keystone())
+        services.extend(self.discover_nova())
+        services.extend(self.discover_neutron())
+        return services
+
+    def discover_keystone(self):
+        """ Uses the Keystone REST API to discover services """
+        # TODO(sc68cal) God help us if someone is using a IPv6 literal in
         # their service catalog
-        host_regex = re.compile("https?://(.*):\d*/v\d.?\d")
-        return host_regex.match(nova_endpoint.internalurl).groups()[0]
+        service_map = []
+        host_regex = re.compile("https?://(.*):\d*")
+
+        self.client = k_client.Client(session=self.session)
+        # TODO(sc68cal) Handle multiple regions and cells
+
+        # Call the REST API once and store the results
+        endpoints = self.client.endpoints.list()
+        services = self.client.services.list()
+
+        # Loop through the endpoints and services, merge into a single map
+        for endpoint in endpoints:
+            for service in services:
+                if service.id == endpoint.service_id:
+                    entry = (host_regex.match(endpoint.internalurl).group(1),
+                             service.name + "-api")
+                    service_map.append(entry)
+        return service_map
 
 
-class NovaDiscovery(object):
-    """ Uses the Nova REST API to discover services """
-    pass
+    def discover_nova(self):
+        """ Uses the Nova REST API to discover agents and their location """
+        self.client = n_client.Client(OS_COMPUTE_API_VERSION,
+                                      session=self.session)
+        return map(lambda service: (service.host, service.binary),
+                   self.client.services.list())
+
+
+    def discover_neutron(self):
+        """ Use the Neutron REST API to discover agents and their location """
+        self.client = mutnauq_client.Client(session=self.session)
+        return map(lambda agent: (agent['binary'], agent['host']),
+                   self.client.list_agents()['agents'])

eaf5cd8139e5395b7ca01cd79d190c350db2e6f1
Sean M. Collins <sean@coreitpro.com>
2016-07-19 09:37:44 -0400

    Add orm relationships and foreign keys to database models
    Also includes unit test skeleton for the DB api tests

---
+++ kostyor/common/constants.py
@@ -0,0 +1,7 @@
+OPENSTACK_VERSIONS = ['liberty', 'mitaka', 'newton']
+
+STATUSES = ['READY FOR UPGRADE',
+            'UPGRADE IN PROGRESS',
+            'UPGRADE PAUSED',
+            'UPGRADE ERROR',
+            'UPGRADE CANCELLED']
--- kostyor/db/sqlalchemy/models.py
+++ kostyor/db/sqlalchemy/models.py
@@ -1,9 +1,14 @@
+from kostyor.common import constants
+
 import sqlalchemy as sa
 from sqlalchemy import orm
+
+from oslo_utils import uuidutils
 
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
 
 class HasId(object):
     """id mixin, add to subclasses that have an id."""
@@ -14,23 +19,37 @@ class HasId(object):
 
 class Cluster(Base, HasId):
     name = sa.Column(sa.String(255))
-    version = sa.Column(sa.String(255))
+    version = sa.Column(sa.Enum(*constants.OPENSTACK_VERSIONS))
 
-    #TODO(sc68cal) change to Enum
-    status = sa.Column(sa.String(255))
+    status = sa.Column(sa.Enum(*constants.STATUSES))
 
 
 class Host(Base, HasId):
 
     hostname = sa.Column(sa.String(255))
+    cluster_id = sa.Column(sa.ForeignKey('cluster.id'))
+
+    cluster = orm.relationship(Cluster, backref=orm.backref("cluster",
+                                                            cascade="delete"))
 
 
 class Service(Base, HasId):
     name = sa.Column(sa.String(255))
-    host = sa.Column(sa.String(255), sa.ForeignKey('Host.id'))
+    host_id = sa.Column(sa.String(255), sa.ForeignKey('host.id'))
 
-    #TODO(sc68cal) Enum?
-    version = sa.Column(sa.String(255))
+    version = sa.Column(sa.Enum(*constants.OPENSTACK_VERSIONS))
+
+    host = orm.relationship(Host,
+                            backref=orm.backref("host", cascade="delete"))
+
 
 class UpgradeTask(Base, HasId):
-    pass
+    cluster_id = sa.Column(sa.ForeignKey('cluster.id'))
+    from_version = sa.Column(sa.Enum(*constants.OPENSTACK_VERSIONS))
+    to_version = sa.Column(sa.Enum(*constants.OPENSTACK_VERSIONS))
+    upgrade_start_time = sa.Column(sa.DateTime)
+    # TODO(sc68cal) Think of a better column name?
+    upgrade_end_time = sa.Column(sa.DateTime)
+
+    cluster = orm.relationship(Cluster, backref=orm.backref("cluster",
+                                                            cascade="delete"))

6381d4dffc641467175a1079a95bf4e0f4cdc9ff
Sean M. Collins <sean@coreitpro.com>
2016-07-21 12:51:27 -0400

    PEP8 fixes for rest_api.py

--- kostyor/rest_api.py
+++ kostyor/rest_api.py
@@ -6,11 +6,9 @@ from db import api as db_api
 from inventory import upgrades
 app = Flask(__name__)
 
+
 def generate_response(status, message):
-    message = {
-            'status': status,
-            'message': message
-    }
+    message = {'status': status, 'message': message}
     resp = jsonify(message)
     resp.status_code = status
     return resp
@@ -26,6 +24,7 @@ def get_cluster_status(cluster_id):
     resp = jsonify(cluster)
     return resp
 
+
 @app.route('/upgrade-status/<cluster_id>')
 def get_upgrade_status(cluster_id):
     upgrade = db_api.get_upgrade_status(cluster_id)
@@ -39,12 +38,14 @@ def get_upgrade_status(cluster_id):
     resp = jsonify(upgrade)
     return resp
 
+
 @app.route('/discovery-methods')
 def get_discovery_methods():
     disc_methods = db_api.get_discovery_methods()
 
     resp = jsonify(disc_methods)
     return resp
+
 
 @app.route('/upgrade-versions/<cluster_id>')
 def get_upgrade_versions(cluster_id):
@@ -59,7 +60,8 @@ def get_upgrade_versions(cluster_id):
     resp = jsonify(versions)
     return resp
 
-@app.route('/discover-cluster', methods = ['POST'])
+
+@app.route('/discover-cluster', methods=['POST'])
 def create_discovery_method():
     discovery_method = request.args.get('method')
     disc_method = db_api.create_discovery_method(discovery_method)
@@ -74,13 +76,14 @@ def create_discovery_method():
     resp.status_code = 201
     return resp
 
-@app.route('/upgrade-cluster/<cluster_id>', methods = ['POST'])
+
+@app.route('/upgrade-cluster/<cluster_id>', methods=['POST'])
 def create_cluster_upgrade(cluster_id):
     to_version = request.args.get('version')
     upgrade = db_api.create_cluster_upgrade(cluster_id, to_version)
     if not upgrade:
         resp = generate_response(
-            404,#TODO probable should fix it later
+            404,  # TODO probable should fix it later
             'Failed to create cluster upgrade for cluster: %s' % cluster_id
         )
         return resp
@@ -89,21 +92,22 @@ def create_cluster_upgrade(cluster_id):
     resp.status_code = 201
     return resp
 
-@app.route('/upgrade-cancel/<cluster_id>', methods = ['PUT'])
+
+@app.route('/upgrade-cancel/<cluster_id>', methods=['PUT'])
 def cancel_cluster_upgrade(cluster_id):
     upgrade = db_api.cancel_cluster_upgrade(cluster_id)
     if not upgrade:
         resp = generate_response(
-            404,#TODO probably should fix it later
+            404,  # TODO probably should fix it later
             'Failed to cancel cluster upgrade for cluster: %s' % cluster_id
         )
         return resp
 
     status = upgrades.cancel_upgrade(cluster_id)
     if status:
-        #TODO: cancel failed, probably should update cluster upgrade status
+        # TODO: cancel failed, probably should update cluster upgrade status
         resp = generate_response(
-            500,#TODO probably should fix it later
+            500,  # TODO probably should fix it later
             'Failed to cancel cluster upgrade for cluster: %s'
             'with message %s' % (cluster_id, status)
         )
@@ -112,21 +116,22 @@ def cancel_cluster_upgrade(cluster_id):
     resp = jsonify(upgrade)
     return resp
 
-@app.route('/upgrade-continue/<cluster_id>', methods = ['PUT'])
+
+@app.route('/upgrade-continue/<cluster_id>', methods=['PUT'])
 def continue_cluster_upgrade(cluster_id):
     upgrade = db_api.continue_cluster_upgrade(cluster_id)
     if not upgrade:
         resp = generate_response(
-            404,#TODO probably should fix it later
+            404,  # TODO probably should fix it later
             'Failed to continue cluster upgrade for cluster: %s' % cluster_id
         )
         return resp
 
     status = upgrades.continue_upgrade(cluster_id)
     if status:
-        #TODO: continue failed, probably should update cluster upgrade status
+        # TODO: continue failed, probably should update cluster upgrade status
         resp = generate_response(
-            500,#TODO probably should fix it later
+            500,  # TODO probably should fix it later
             'Failed to continue cluster upgrade for cluster %s '
             'with message %s' % (cluster_id, status)
         )
@@ -135,21 +140,22 @@ def continue_cluster_upgrade(cluster_id):
     resp = jsonify(upgrade)
     return resp
 
-@app.route('/upgrade-pause/<cluster_id>', methods = ['PUT'])
+
+@app.route('/upgrade-pause/<cluster_id>', methods=['PUT'])
 def pause_cluster_upgrade(cluster_id):
     upgrade = db_api.pause_cluster_upgrade(cluster_id)
     if not upgrade:
         resp = generate_response(
-            404,#TODO probably should fix it later
+            404,  # TODO probably should fix it later
             'Failed to pause cluster upgrade for cluster: %s' % cluster_id
         )
         return resp
 
     status = upgrades.pause_upgrade(cluster_id)
     if status:
-        #TODO: pause failed, probably should update cluster upgrade status
+        # TODO: pause failed, probably should update cluster upgrade status
         resp = generate_response(
-            500,#TODO probably should fix it later
+            500,  # TODO probably should fix it later
             'Failed to pause cluster upgrade for cluster %s with message %s'
             % (cluster_id, status)
         )
@@ -158,21 +164,22 @@ def pause_cluster_upgrade(cluster_id):
     resp = jsonify(upgrade)
     return resp
 
-@app.route('/upgrade-rollback/<cluster_id>', methods = ['PUT'])
+
+@app.route('/upgrade-rollback/<cluster_id>', methods=['PUT'])
 def rollback_cluster_upgrade(cluster_id):
     upgrade = db_api.rollback_cluster_upgrade(cluster_id)
     if not upgrade:
         resp = generate_response(
-            404,#TODO probably should fix it later
+            404,  # TODO probably should fix it later
             'Failed to rollback cluster upgrade for cluster %s' % cluster_id
         )
         return resp
 
     status = upgrades.rollback_upgrade(cluster_id)
     if status:
-        #TODO: rollback failed, probably should update cluster upgrade status
+        # TODO: rollback failed, probably should update cluster upgrade status
         resp = generate_response(
-            500,#TODO probably should fix it later
+            500,  # TODO probably should fix it later
             'Failed to rollback cluster upgrade for cluster: %s '
             'with message %s' % (cluster_id, status)
         )

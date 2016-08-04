0accedc150641a20877030c6acbc8c94a9d6d842
Sean M. Collins <sean@coreitpro.com>
2016-08-03 17:07:37 -0700

    [WIP] Add "context" arg to all DB API calls
    This is used to pass in a Database Context object,
    which contains all the required SQLAlchemy objects
    (session & engine).

--- kostyor/db/api.py
+++ kostyor/db/api.py
@@ -1,14 +1,7 @@
-"""
-    return {'items': [{'id': cluster_id,
-                       'name': 'tmp',
-                       'status': 'ready'},
-                      {'id': '9871',
-                       'name': 'tmp2',
-                       'status': 'updating'}]}
-"""
+from kostyor.db.sqlalchemy import models
 
 
-def get_cluster_status(cluster_id):
+def get_cluster_status(context, cluster_id):
     # TODO fix it later
     return {'id': cluster_id,
             'name': "Sean's Lab",
@@ -16,54 +9,55 @@ def get_cluster_status(cluster_id):
             'status': "READY"}
 
 
-def get_upgrade_status(cluster_id):
-    return {'id': cluster_id, 'status': 'upgrading'}
+def get_upgrade_status(context, cluster_id):
+    u_task = context.session.query(models.UpgradeTask).get(cluster_id)
+    return u_task.to_dict()
 
 
-def get_discovery_methods():
+def get_discovery_methods(context):
     return {'items': [{'method': 'method1'}, {'method': 'method2'}]}
 
 
-def get_upgrade_versions(cluster_id):
+def get_upgrade_versions(context, cluster_id):
     return {'items': [{'version': 'version1'}, {'version': 'version2'}]}
 
 
-def create_discovery_method(method):
+def create_discovery_method(context, method):
     return {'id': '1', 'method': method}
 
 
-def create_cluster_upgrade(cluster_id, to_version):
+def create_cluster_upgrade(context, cluster_id, to_version):
     return {'id': cluster_id, 'status': 'upgrading'}
 
 
-def cancel_cluster_upgrade(cluster_id):
+def cancel_cluster_upgrade(context, cluster_id):
     return {'id': cluster_id, 'status': 'canceling'}
 
 
-def continue_cluster_upgrade(cluster_id):
+def continue_cluster_upgrade(context, cluster_id):
     return {'id': cluster_id, 'status': 'upgrading'}
 
 
-def pause_cluster_upgrade(cluster_id):
+def pause_cluster_upgrade(context, cluster_id):
     return {'id': cluster_id, 'status': 'paused'}
 
 
-def rollback_cluster_upgrade(cluster_id):
+def rollback_cluster_upgrade(context, cluster_id):
     return {'id': cluster_id, 'status': 'rolling back'}
 
 
-def get_clusters():
+def get_clusters(context):
     return {'clusters': [{'id': 'TEST', 'name': 'Fake Cluster', 'status':
                           'READY'}]}
 
 
-def create_host(name, cluster_id):
+def create_host(context, name, cluster_id):
     return {'id': '1234',
             'name': name,
             'cluster_id': cluster_id}
 
 
-def create_service(name, host_id, version):
+def create_service(context, name, host_id, version):
     return {'id': '4321',
             'name': name,
             'host_id': host_id,

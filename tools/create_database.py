55cfaaecb2fa044bc26bb0c10a7a8364ce19b21b
Sean M. Collins <sean@coreitpro.com>
2016-08-17 16:10:31 -0400

    Create some sample data and commit it to the DB

--- tools/create_database.py
+++ tools/create_database.py
@@ -1,7 +1,13 @@
 #!user/bin/env python
 
+from kostyor.common import constants
 from kostyor.db import models
+from kostyor.db import api
 from kostyor.db.api import db_session
 
 
 models.Base.metadata.create_all(db_session.bind)
+
+api.create_cluster(db_session, "test", constants.MITAKA,
+                   constants.READY_FOR_UPGRADE)
+db_session.commit()

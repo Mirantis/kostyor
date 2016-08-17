b03cd50f7fab5240bdb5119b28488c07f85222d4
Sean M. Collins <sean@coreitpro.com>
2016-08-17 10:03:02 -0400

    Install kostyor-cli in /tmp, then run
    Setuptools still gets confused if you clone it inside
    of another project and run the setup.py file.

--- tools/run_integration.sh
+++ tools/run_integration.sh
@@ -2,16 +2,32 @@
 
 # install the REST API and CLI
 
+KOSTYOR_DIR=$(pwd)
+
+export KOSTYOR_PORT=5000
+
 python setup.py install
 
-python kostyor/cli/setup.py install
+if ! [ -d /tmp/kostyor-cli ]; then
+    git clone https://github.com/sc68cal/Kostyor-cli.git -c /tmp/kostyor-cli
+else
+    git -C /tmp/kostyor-cli pull
+fi
+
+cd /tmp/kostyor-cli
+
+python /tmp/kostyor-cli/setup.py install
 
 # Start the REST API
 
+cd $KOSTYOR_DIR
+
 screen -S kostyor -d -m python kostyor/rest_api.py
+
+sleep 5
 
 kostyor cluster-list
 
 kostyor cluster-status TEST
 
-screen -X -S kostyor quit
+pkill -f 'python kostyor/rest_api.py'

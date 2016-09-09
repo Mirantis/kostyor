6315fb876786683903944648d9f0615d66493732
Igor Kalnitsky <igor@kalnitsky.org>
2016-09-09 13:04:33 +0300

    Fix /discovery-methods endpoint
    Currently there're two issues with that endpoint:
    
    * It uses DBAPI.get_discovery_methods() without arguments but it
      does receive one.
    
    * It expects DBAPI.get_discovery_methods() to return a list of
      supported discovery methods, but it returns a dictionary instead.
    
    The fix for the first one is to get rid of unused argument, and for the
    second one - return a list instead of dictionary. The funny thing is, we
    have a correct mock in tests.. it's a production code that's  broken.

--- kostyor/db/api.py
+++ kostyor/db/api.py
@@ -40,8 +40,8 @@ def get_upgrade(upgrade_id):
     return u_task.to_dict()
 
 
-def get_discovery_methods(db_session):
-    return {'discovery_methods': constants.DISCOVERY_METHODS}
+def get_discovery_methods():
+    return constants.DISCOVERY_METHODS
 
 
 def get_upgrade_versions(cluster_id):

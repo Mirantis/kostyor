e8d87900d5591a557ca4d88e6eea6e0959460085
Sean M. Collins <sean@coreitpro.com>
2016-09-23 12:58:10 -0400

    [WIP] Upgrade Driver model
    Create a driver model for upgrade driver, and introduce some of the pre and post hooks that can be called
    by the decision engine.

---
+++ kostyor/upgrades/driver.py
@@ -0,0 +1,54 @@
+import abc
+import six
+
+
+@six.add_metaclass(abc.ABCMeta)
+class UpgradeDriver():
+    
+    @abc.abstractmethod
+    def pre_host_upgrade_hook(self, host):
+    """Called by the decision engine before a host is upgraded,
+    allows an UpgradeDriver the opportunity to do any operations required
+    before the host is upgraded
+    
+    Params
+    
+    host: a kostyor.db.models.Host instance, representing the host that is scheduled to be upgraded
+    """
+        pass
+        
+    @abc.abstractmethod
+    def pre_service_upgrade_hook(self, service):
+    """Called by the decision engine before a service is upgraded,
+    allows an UpgradeDriver the opportunity to do any operations required
+    before a service is upgraded
+    
+    Params
+    
+    service: a kostyor.db.models.Service instance, representing the host that is scheduled to be upgraded
+    """
+        pass
+        
+    @abc.abstractmethod
+    def post_host_upgrade_hook(self, host):
+    """Called by the decision engine after a host is upgraded,
+    allows an UpgradeDriver the opportunity to do any operations required
+    after the host is upgraded
+    
+    Params
+    
+    host: a kostyor.db.models.Host instance, representing the host that is scheduled to be upgraded
+    """
+        pass
+        
+    @abc.abstractmethod
+    def post_service_upgrade_hook(self, service):
+    """Called by the decision engine after a service is upgraded,
+    allows an UpgradeDriver the opportunity to do any operations required
+    after a service is upgraded
+    
+    Params
+    
+    service: a kostyor.db.models.Service instance, representing the host that is scheduled to be upgraded
+    """
+        pass

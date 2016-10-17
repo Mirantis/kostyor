9fcb4dd2aa99c54c469fe7a112792d46b8e4ae5d
Sean M. Collins <sean@coreitpro.com>
2016-10-17 14:03:26 -0400

    Switch over to Alembic for integration tests
    Fixes #35

--- tools/create_initial_data.py
+++ tools/create_initial_data.py
@@ -2,14 +2,11 @@
 
 from kostyor.common import constants
 from kostyor import conf
-from kostyor.db import models
 from kostyor.db import api
 
 
 conf.parse_args()
 api.configure_session(conf.CONF.database.connection)
-
-models.Base.metadata.create_all(api.db_session.bind)
 
 api.create_cluster("test", constants.MITAKA,
                    constants.READY_FOR_UPGRADE)

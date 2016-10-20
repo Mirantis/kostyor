d5893d1e4aaba2a09e1268f9ae481e4af684452c
Ihor Kalnytskyi <igor@kalnitsky.org>
2016-10-20 16:14:48 +0300

    Align UpgradeDriver interface with UpgradeEngine
    There are several misalignments between Driver interface and how the
    Engine consumes it. Here they are:
    
    * The main methods of UpgradeDriver interface receive upgrade task as a
      param. However, according to UpgradeEngine implementation they receive
      an service instance scheduled to be upgraded (as a dict). But the truth
      is in the midle, and both parameters must be passed and received
      instead.
    
    * Driver's pre/post hooks do not receive an upgrade task instance, so they
      do not have a context while they might want to.
    
    * Driver's post hooks aren't consumed by engine at all.
    
    * Driver's pre/post hooks return a Celery task but it's ignored by the
      Engine.
    
    All these misalignments are resolved within a single commit since
    otherwise the fix will be partial, looks weird and need to undo/redo
    existing tests. Having those in a single commit allows to only
    increase coverage without decreasing.
    
    Co-Authored-By: Sean M. Collins <sean@coreitpro.com>

--- kostyor/upgrades/driver.py
+++ kostyor/upgrades/driver.py
@@ -14,115 +14,138 @@ class UpgradeDriver():
         """
         pass
 
-    def pre_host_upgrade_hook(self, host):
+    def pre_host_upgrade_hook(self, upgrade_task, host):
         """Called by the decision engine before a host is upgraded,
         allows an UpgradeDriver the opportunity to do any operations required
         before the host is upgraded
 
-        Params
+        :param host: a host that is scheduled to be upgraded
+        :type host: a kostyor.db.models.Host instance as a dict
 
-        host: a kostyor.db.models.Host instance, representing the host that is
-        scheduled to be upgraded
+        :param upgrade_task: the upgrade task that the engine is performing
+        :type upgrade_task: kostyor.db.models.UpgradeTask instance
         """
         return tasks.noop.si()
 
-    def pre_service_upgrade_hook(self, service):
+    def pre_service_upgrade_hook(self, upgrade_task, service):
         """Called by the decision engine before a service is upgraded,
         allows an UpgradeDriver the opportunity to do any operations required
         before a service is upgraded
 
-        Params
+        :param service: a service that is scheduled to be upgraded
+        :type service: a kostyor.db.models.Service instance as a dict
 
-        service: a kostyor.db.models.Service instance, representing the service
-        that is scheduled to be upgraded
+        :param upgrade_task: the upgrade task that the engine is performing
+        :type upgrade_task: kostyor.db.models.UpgradeTask instance
         """
         return tasks.noop.si()
 
-    def post_host_upgrade_hook(self, host):
+    def post_host_upgrade_hook(self, upgrade_task, host):
         """Called by the decision engine after a host is upgraded,
         allows an UpgradeDriver the opportunity to do any operations required
         after the host is upgraded
 
-        Params
+        :param host: a host that is scheduled to be upgraded
+        :type host: a kostyor.db.models.Service instance as a dict
 
-        host: a kostyor.db.models.Host instance, representing the host that is
-        scheduled to be upgraded
+        :param upgrade_task: the upgrade task that the engine is performing
+        :type upgrade_task: kostyor.db.models.UpgradeTask instance
         """
         return tasks.noop.si()
 
-    def post_service_upgrade_hook(self, service):
+    def post_service_upgrade_hook(self, upgrade_task, service):
         """Called by the decision engine after a service is upgraded,
         allows an UpgradeDriver the opportunity to do any operations required
         after a service is upgraded
 
-        Params
+        :param service: a service that is scheduled to be upgraded
+        :type service: a kostyor.db.models.Service instance as a dict
 
-        service: a kostyor.db.models.Service instance, representing the service
-        that is scheduled to be upgraded
+        :param upgrade_task: the upgrade task that the engine is performing
+        :type upgrade_task: kostyor.db.models.UpgradeTask instance
         """
         return tasks.noop.si()
 
     @abc.abstractmethod
-    def cancel_upgrade(self, upgrade_task):
+    def cancel_upgrade(self, upgrade_task, service):
         """Called by the decision engine to cancel an upgrade that is in
-        progress"""
+        progress
+
+        :param service: a service that is scheduled to be upgraded
+        :type service: a kostyor.db.models.Service instance as a dict
+
+        :param upgrade_task: the upgrade task that the engine is performing
+        :type upgrade_task: kostyor.db.models.UpgradeTask instance
+        """
         pass
 
     @abc.abstractmethod
-    def rollback_upgrade(self, upgrade_task):
+    def rollback_upgrade(self, upgrade_task, service):
         """Called by the decision engine to rollback a running upgrade
 
-        Params
+        :param service: a service that is scheduled to be upgraded
+        :type service: a kostyor.db.models.Service instance as a dict
 
-        upgrade_task: A kostyor.db.models.UpgradeTask instance"""
+        :param upgrade_task: the upgrade task that the engine is performing
+        :type upgrade_task: kostyor.db.models.UpgradeTask instance
+        """
         pass
 
     @abc.abstractmethod
-    def start_upgrade(self, upgrade_task):
+    def start_upgrade(self, upgrade_task, service):
         """Called by the decision engine to start an upgrade
 
-        Params
+        :param service: a service that is scheduled to be upgraded
+        :type service: a kostyor.db.models.Service instance as a dict
 
-        upgrade_task: A kostyor.db.models.UpgradeTask instance"""
+        :param upgrade_task: the upgrade task that the engine is performing
+        :type upgrade_task: kostyor.db.models.UpgradeTask instance
+        """
         pass
 
     @abc.abstractmethod
-    def stop_upgrade(self, upgrade_task):
+    def stop_upgrade(self, upgrade_task, service):
         """Called by the decision engine to stop a running upgrade
 
-        Params
+        :param service: a service that is scheduled to be upgraded
+        :type service: a kostyor.db.models.Service instance as a dict
 
-        upgrade_task: A kostyor.db.models.UpgradeTask instance"""
+        :param upgrade_task: the upgrade task that the engine is performing
+        :type upgrade_task: kostyor.db.models.UpgradeTask instance
+        """
         pass
 
     @abc.abstractmethod
-    def pause_upgrade(self, upgrade_task):
+    def pause_upgrade(self, upgrade_task, service):
         """Called by the decision engine to pause a running upgrade
 
-        Params
+        :param service: a service that is scheduled to be upgraded
+        :type service: a kostyor.db.models.Service instance as a dict
 
-        upgrade_task: A kostyor.db.models.UpgradeTask instance"""
+        :param upgrade_task: the upgrade task that the engine is performing
+        :type upgrade_task: kostyor.db.models.UpgradeTask instance
+        """
         pass
 
 
 class NoOpDriver(UpgradeDriver):
 
-    def stop_upgrade(self, upgrade_task):
+    def stop_upgrade(self, upgrade_task, service):
         return tasks.noop.si()
 
-    def start_upgrade(self, upgrade_task):
+    def start_upgrade(self, upgrade_task, service):
         return tasks.noop.si()
 
-    def pause_upgrade(self, upgrade_task):
+    def pause_upgrade(self, upgrade_task, service):
         return tasks.noop.si()
 
-    def cancel_upgrade(self, upgrade_task):
+    def cancel_upgrade(self, upgrade_task, service):
         return tasks.noop.si()
 
-    def rollback_upgrade(self, upgrade_task):
+    def rollback_upgrade(self, upgrade_task, service):
         return tasks.noop.si()
 
-    def continue_upgrade(self, upgrade_task):
+    def continue_upgrade(self, upgrade_task, service):
         return tasks.noop.si()
 
     def supports_upgrade_rollback(self):

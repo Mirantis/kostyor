78a2504a9a91ab8519442d664af151474ed36737
Julia Varlamova <jvarlamova@mirantis.com>
2016-10-21 12:18:19 +0300

    Improve integration testing
    Add more commands to Kostyor integration testing script:
    
      check-upgrade
      upgrade-start
      upgrade-list
      host-list
      service-list

--- tools/create_initial_data.py
+++ tools/create_initial_data.py
@@ -8,6 +8,10 @@ from kostyor.db import api
 conf.parse_args()
 api.configure_session(conf.CONF.database.connection)
 
-api.create_cluster("test", constants.MITAKA,
-                   constants.READY_FOR_UPGRADE)
+cluster = api.create_cluster("test", constants.MITAKA,
+                             constants.READY_FOR_UPGRADE)
+
+host = api.create_host("test-host", cluster["id"])
+api.create_service("test-service", host['id'], constants.MITAKA)
+
 api.db_session.commit()
